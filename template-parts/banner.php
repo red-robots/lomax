ba1eafc8e3e6cda4ca49c96bca6d8adc81fc433a
Lisa <annalisa.quiamco@gmail.com>
2019-08-07 22:29:40 -0600

    Added theme files

---
+++ template-parts/banner.php
@@ -0,0 +1,33 @@
+<?php
+if ( is_front_page() || is_home() ) { 
+	$slides = get_field('slides'); 
+	$slide_class = '';
+	if($slides) {
+		$count = count($slides);
+		if($count>1) {
+			$slide_class = ' flexslider';
+		}
+	}
+	?>
+	<?php if ($slides) { ?>
+	<div class="home-sliders">
+		<div class="slideshow<?php echo $slide_class ?>">
+			<ul class="slides">
+				<?php foreach ($slides as $e) { ?>
+				<li class="slide-item">
+					<img src="<?php echo $e['url']; ?>" alt="<?php echo $e['title']; ?>" />
+				</li>	
+				<?php } ?>
+			</ul>
+		</div>
+	</div>
+	<?php } ?>
+	
+<?php } else {
+	$banner = get_field('banner');
+	if($banner) { ?>
+	<div class="subpage-banner clear">
+		<img src="<?php echo $banner['url'] ?>" alt="<?php echo $banner['title'] ?>" />
+	</div>
+	<?php } ?>
+<?php } ?>
