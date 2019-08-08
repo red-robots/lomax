3af8129288a5b452736e930c2e37ebfecf44bb50
Lisa <annalisa.quiamco@gmail.com>
2019-08-07 22:58:07 -0600

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -7,7 +7,7 @@ get_header(); ?>
 		<?php
 		while ( have_posts() ) : the_post();
 
-			get_template_part( 'template-parts/content', 'page' );
+			//get_template_part( 'template-parts/content', 'page' );
 
 		endwhile; // End of the loop.
 		?>
