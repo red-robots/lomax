27d7abde442845cc758d6220c3f698c3b2a35f92
Lisa <annalisa.quiamco@gmail.com>
2019-08-08 22:17:59 -0600

    Added testimonials post type

--- inc/post-types.php
+++ inc/post-types.php
@@ -13,6 +13,14 @@ function js_custom_init() {
             'menu_icon' => 'dashicons-location',
             'supports'  => array('title','editor','thumbnail')
         ),
+        array(
+            'post_type' => 'testimonials',
+            'menu_name' => 'Testimonials',
+            'plural'    => 'Testimonials',
+            'single'    => 'Testimonial',
+            'menu_icon' => 'dashicons-format-quote',
+            'supports'  => array('title','editor')
+        ),
     );
     
     if($post_types) {
