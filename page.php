d265af5e2f214bd1464b807c84574ba68919ab8f
Lisa <annalisa.quiamco@gmail.com>
2019-08-13 21:10:56 -0600

    Setup default page with featured image

--- page.php
+++ page.php
@@ -12,19 +12,31 @@
  * @package ACStarter
  */
 
+
 get_header(); ?>
 
 	<div id="primary" class="full-content-area clear default-content">
 		<main id="main" class="site-main wrapper clear" role="main">
-
-			<?php
-			while ( have_posts() ) : the_post();
-
-				get_template_part( 'template-parts/content', 'page' );
-
-			endwhile; // End of the loop.
+		<?php while ( have_posts() ) : the_post(); ?>
+			<?php 
+				$img = get_field('featured_image'); 
+				$contentClass = ($img) ? 'has-image':'no-image';
 			?>
+			<header class="entry-header">
+				<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+			</header>
 
+			<article id="post-<?php the_ID(); ?>" <?php post_class($contentClass); ?>>
+				<div class="entry-content"><?php the_content(); ?></div>
+			</article>
+
+			<?php if ($img) { ?>
+			<div class="featuredImg">
+				<img src="<?php echo $img['url'] ?>" alt="<?php echo $img['title'] ?>" />
+			</div>	
+			<?php } ?>
+
+		<?php endwhile;  ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
